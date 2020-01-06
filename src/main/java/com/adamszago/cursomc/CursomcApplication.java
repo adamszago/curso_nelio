f042be966cd2e226c8ce97d20709f9ddd0e5ab2b
adamszago <adams.zago@gmail.com>
2020-01-06 18:41:39 -0300

    Instanciacao de objetos

--- src/main/java/com/adamszago/cursomc/CursomcApplication.java
+++ src/main/java/com/adamszago/cursomc/CursomcApplication.java
@@ -1,13 +1,33 @@
 package com.adamszago.cursomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.adamszago.cursomc.domain.Categoria;
+import com.adamszago.cursomc.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class CursomcApplication {
+public class CursomcApplication implements CommandLineRunner {
+
+	@Autowired
+	CategoriaRepository repoCategoria;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+
+		repoCategoria.saveAll(Arrays.asList(cat1, cat2));
+		
+	}
+
+	
 }
